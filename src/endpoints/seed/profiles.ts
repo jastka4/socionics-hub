8698ae6a3b8b5d0474a75fc523c1c6ca68e2b3af
Justyna Skalska <jastka4@gmail.com>
2024-09-24 17:19:44 +0200

    Add officially typed SCS ESEs

--- src/endpoints/seed/profiles.ts
+++ src/endpoints/seed/profiles.ts
@@ -94,6 +94,16 @@ export const profiles: ProfileSeed[] = [
     profilePicture: '{{IMAGE}}',
   },
   {
+    slug: 'andrei-bitov',
+    name: 'Andrei Bitov',
+    category: '{{CATEGORY_REAL}}',
+    SHS: {},
+    SCS: {
+      type: 'ESE',
+    },
+    profilePicture: '{{IMAGE}}',
+  },
+  {
     slug: 'andrew-tate',
     name: 'Andrew Tate',
     category: '{{CATEGORY_REAL}}',
@@ -202,12 +212,32 @@ export const profiles: ProfileSeed[] = [
     profilePicture: '{{IMAGE}}',
   },
   {
+    slug: 'george-washington',
+    name: 'George Washington',
+    category: '{{CATEGORY_REAL}}',
+    SHS: {},
+    SCS: {
+      type: 'ESE',
+    },
+    profilePicture: '{{IMAGE}}',
+  },
+  {
     slug: 'georgi-plekhanov',
     name: 'Georgi Plekhanov',
     category: '{{CATEGORY_REAL}}',
     SHS: {},
     SCS: {
       type: 'ILE',
+    },
+    profilePicture: '{{IMAGE}}',
+  },
+  {
+    slug: 'gerard-philipe',
+    name: 'Gérard Philipe',
+    category: '{{CATEGORY_REAL}}',
+    SHS: {},
+    SCS: {
+      type: 'ESE',
     },
     profilePicture: '{{IMAGE}}',
   },
@@ -272,6 +302,16 @@ export const profiles: ProfileSeed[] = [
     profilePicture: '{{IMAGE}}',
   },
   {
+    slug: 'jules-verne',
+    name: 'Jules Verne',
+    category: '{{CATEGORY_REAL}}',
+    SHS: {},
+    SCS: {
+      type: 'ESE',
+    },
+    profilePicture: '{{IMAGE}}',
+  },
+  {
     slug: 'konstantin-tsiolkovsky',
     name: 'Konstantin Tsiolkovsky',
     category: '{{CATEGORY_REAL}}',
@@ -328,6 +368,26 @@ export const profiles: ProfileSeed[] = [
     SHS: {},
     SCS: {
       type: 'ILE',
+    },
+    profilePicture: '{{IMAGE}}',
+  },
+  {
+    slug: 'mikhail-bakunin',
+    name: 'Mikhail Bakunin',
+    category: '{{CATEGORY_REAL}}',
+    SHS: {},
+    SCS: {
+      type: 'ESE',
+    },
+    profilePicture: '{{IMAGE}}',
+  },
+  {
+    slug: 'mikhail-kozakov',
+    name: 'Mikhail Kozakov',
+    category: '{{CATEGORY_REAL}}',
+    SHS: {},
+    SCS: {
+      type: 'ESE',
     },
     profilePicture: '{{IMAGE}}',
   },
@@ -452,12 +512,42 @@ export const profiles: ProfileSeed[] = [
     profilePicture: '{{IMAGE}}',
   },
   {
+    slug: 'prosper-merimee',
+    name: 'Prosper Mérimée',
+    category: '{{CATEGORY_REAL}}',
+    SHS: {},
+    SCS: {
+      type: 'ESE',
+    },
+    profilePicture: '{{IMAGE}}',
+  },
+  {
     slug: 'sigmund-freud',
     name: 'Sigmund Freud',
     category: '{{CATEGORY_REAL}}',
     SHS: {},
     SCS: {
       type: 'ILE',
+    },
+    profilePicture: '{{IMAGE}}',
+  },
+  {
+    slug: 'valentin-gaft',
+    name: 'Valentin Gaft',
+    category: '{{CATEGORY_REAL}}',
+    SHS: {},
+    SCS: {
+      type: 'ESE',
+    },
+    profilePicture: '{{IMAGE}}',
+  },
+  {
+    slug: 'victor-hugo',
+    name: 'Victor Hugo',
+    category: '{{CATEGORY_REAL}}',
+    SHS: {},
+    SCS: {
+      type: 'ESE',
     },
     profilePicture: '{{IMAGE}}',
   },
